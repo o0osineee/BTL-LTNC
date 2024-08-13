38c06efaffef764b6da79a432367b1cd4b533e54
o0osineee <144638450+o0osineee@users.noreply.github.com>
2024-08-13 17:22:24 +0700

    Delete include directory

--- include/RandomNumberGenerator.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef RANDOM_NUMBER_GENERATOR_IG
-#define RANDOM_NUMBER_GENERATOR_IG
-
-typedef unsigned short int us;
-
-// function for generating random number
-us generateRandomNumber(us, us);
-
-#endif
